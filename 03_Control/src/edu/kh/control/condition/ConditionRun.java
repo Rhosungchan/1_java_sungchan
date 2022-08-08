ad5294222ab7d2819f2f96d99c5004785875b8fe
skclsso25 <110643104+Rhosungchan@users.noreply.github.com>
2022-08-08 19:46:43 +0900

    수업후 보강 및 수정

--- 03_Control/src/edu/kh/control/condition/ConditionRun.java
+++ 03_Control/src/edu/kh/control/condition/ConditionRun.java
@@ -1,21 +1,21 @@
 package edu.kh.control.condition;
 
+//코드를 실행시키는 부분(시키는 사람)
 public class ConditionRun {
-   //코드를 실행시키는 부분(시키는사람)
-   public static void main(String[] args) {
-      
-      
-      ConditionExample ex = new ConditionExample();
-      
-      //커서놓고 → 컨트롤 + / ==한줄 주석
-      //ex.test1();
-      //ex.test2();
-      ex.ex1();
-      ex.ex2();
-      
-      
-      // 다시 듣기 놓침!!
-   }
 
+	public static void main(String[] args) {
+		
+		ConditionExample ex = new ConditionExample();
+		
+		// 커서놓고 -> ctrl + / ==> 한줄주석
+		// ex.test1();
+		// ex.test2();
+		
+		// ex.ex1();
+		ex.ex2();
+		
+		
+	}
 }
+
 
